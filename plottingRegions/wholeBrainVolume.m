1794e23310985c8c6e5cbac1f419cad0910d9267
ghpetty <ghp2117@columbia.edu>
2023-04-24 18:34:44 -0400

    Added volume downsampling step to reduce memory usage and speed up isosurface computation

--- plottingRegions/wholeBrainVolume.m
+++ plottingRegions/wholeBrainVolume.m
@@ -21,7 +21,7 @@ addRequired(p,'annotationVolume');
 addParameter(p,'Hemisphere','both',...
     @(x) ismember(lower(x),{'all','both','left','right'}))
 addParameter(p,'ReductionFactor',0.01,@(x) isnumeric(x) && isscalar(x) && x<=1 && x>0);
-
+addParameter(p,'DoFill',true)
 parse(p,annotationVolume,varargin{:});
 
 midlineValue = 570; % Hardcoding this, from inspection of 3D brain volume visualization
@@ -32,7 +32,7 @@ midlineValue = 570; % Hardcoding this, from inspection of 3D brain volume visual
 disp('   Indexing annotation volume...');
 volumeBool = annotationVolume ~=1;
 
-volumeBool = permute(volumeBool,[3 1 2]);  % Reshapes matrix to fit with wireframe diagram
+volumeBool = permute(volumeBool,[3 1 2]);
 % Isolate specified hemisphere by setting all values on the opposite
 % hemisphere to 'false'
 switch string(lower(p.Results.Hemisphere))
@@ -46,54 +46,69 @@ end
 % figure;
 % sp1 = subplot(1,2,1);
 % sp2 = subplot(1,2,2);
-volumeY = size(volumeBool,2);
-volumeX = size(volumeBool,1);
-volumeZ = size(volumeBool,3);
-disp('Patching gaps sagittally');
-parfor i = 1:volumeX
-    currentSlice =imadjust(double(squeeze(volumeBool(i,:,:))))';
-%     if ~all(currentSlice == 0,'all')
-%     imshow(currentSlice,'Parent',sp1);
-    % Imfill to try and patch over gaps within regions
-    csFill = imfill(currentSlice,'holes');
-%     imshow(csFill,'Parent',sp2);
-%     drawnow
-    volumeBool(i,:,:) = (csFill == 1)' ;
-%     drawnow;
-%     
-end
-disp('Patching gaps coronally');
-parfor i = 1:volumeY
-    currentSlice =imadjust(double(squeeze(volumeBool(:,i,:))))';
-%     if ~all(currentSlice == 0,'all')
-%     imshow(currentSlice,'Parent',sp1);
-    % Imfill to try and patch over gaps within regions
-    csFill = imfill(currentSlice,'holes');
-%     imshow(csFill,'Parent',sp2);
-%     drawnow
-    volumeBool(:,i,:) = (csFill == 1)' ;
-%     end
-end
-disp('Patching gaps transversely');
-parfor i = 1:volumeZ
-    currentSlice =imadjust(double(squeeze(volumeBool(:,:,i))));
-%     if ~all(currentSlice == 0,'all')
-%     imshow(currentSlice,'Parent',sp1);
-    % Imfill to try and patch over gaps within regions
-    csFill = imfill(currentSlice,'holes');
-%     imshow(csFill,'Parent',sp2);
-%     drawnow
-    volumeBool(:,:,i) = (csFill == 1);
-%     end
+
+
+
+if p.Results.DoFill
+    volumeY = size(volumeBool,2);
+    volumeX = size(volumeBool,1);
+    volumeZ = size(volumeBool,3);
+    disp('Patching gaps sagittally');
+    for i = 1:volumeX
+        currentSlice =imadjust(double(squeeze(volumeBool(i,:,:))))';
+        %     if ~all(currentSlice == 0,'all')
+        %     imshow(currentSlice,'Parent',sp1);
+        % Imfill to try and patch over gaps within regions
+        csFill = imfill(currentSlice,'holes');
+        %     imshow(csFill,'Parent',sp2);
+        %     drawnow
+        volumeBool(i,:,:) = (csFill == 1)' ;
+        %     drawnow;
+        inlinePercent_bv(i,volumeX,1);
+    end
+    disp('Patching gaps coronally');
+    for i = 1:volumeY
+        currentSlice =imadjust(double(squeeze(volumeBool(:,i,:))))';
+        %     if ~all(currentSlice == 0,'all')
+        %     imshow(currentSlice,'Parent',sp1);
+        % Imfill to try and patch over gaps within regions
+        csFill = imfill(currentSlice,'holes');
+        %     imshow(csFill,'Parent',sp2);
+        %     drawnow
+        volumeBool(:,i,:) = (csFill == 1)' ;
+        %     end
+        inlinePercent_bv(i,volumeY,1);
+    end
+    disp('Patching gaps transversely');
+    for i = 1:volumeZ
+        currentSlice =imadjust(double(squeeze(volumeBool(:,:,i))));
+        %     if ~all(currentSlice == 0,'all')
+        %     imshow(currentSlice,'Parent',sp1);
+        % Imfill to try and patch over gaps within regions
+        csFill = imfill(currentSlice,'holes');
+        %     imshow(csFill,'Parent',sp2);
+        %     drawnow
+        volumeBool(:,:,i) = (csFill == 1);
+        %     end
+        inlinePercent_bv(i,volumeZ,1);
+    end
 end
 
+% Downsample the volume to reduce memory and simplify the process of
+% finding isosurface
+downsampleIndices = {1:2:size(volumeBool,1)
+    1:2:size(volumeBool,2)
+    1:2:size(volumeBool,3)};
+downsampleVolume = volumeBool(downsampleIndices{1},downsampleIndices{2},downsampleIndices{3});
 
 disp('Computing volume surface (this will take several minutes)');
-ISO = isosurface(volumeBool,0.5);
+ISO = isosurface(downsampleVolume,0.5);
 if p.Results.ReductionFactor ~=1
     disp('   Simplifying surface');
     ISO = reducepatch(ISO,0.01);
 end
+% Scale the vertices back up so they line up with reference space
+ISO.vertices = ISO.vertices * 2;
 volume = ISO;
 disp('Done!');
 % Plotting code for debugging:
